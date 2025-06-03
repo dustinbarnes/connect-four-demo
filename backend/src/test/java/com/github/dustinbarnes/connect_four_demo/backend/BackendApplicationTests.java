7ee154fefaeeeb604e100a37069f124e1275c92e
Dustin Barnes <dustin.barnes@gmail.com>
2025-06-03 12:14:57 -0700

    Spring Initializr-created Backend service

---
+++ backend/src/test/java/com/github/dustinbarnes/connect_four_demo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dustinbarnes.connect_four_demo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
